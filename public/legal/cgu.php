e54ff1dc10ff1706ab7a01735090eae790bf660b
Maël Debon <45589315+maeldebon@users.noreply.github.com>
2020-02-11 14:15:15 +0100

    Add Terms of Use (#2)
    * Add Terms of Use
    
    * Export CSS in another file
    
    * Change owner

---
+++ public/legal/cgu.php
@@ -0,0 +1,63 @@
+<link rel="stylesheet" type="text/css" href="main.css">
+
+<h2>Conditions Générales d’Utilisation</h2>
+
+<div class="main">
+    <h4>I. A propos de nous</h4>
+    <div class="text">
+        <p>La Société 5C, SAS, au capital de 50 000 euros, dont le siège social est à ORVAULT (44700) 213 Route de Rennes, immatriculée au registre du commerce et des sociétés de ORVAULT sous le numéro 123 456 789 représentée par Monsieur Johan DELEON (ci-après la " Société ”).</p>
+
+        <p>La Société propose les services suivants : Fournir une application mobile permettant aux randonneurs de localiser et visiter facilement des lieux insolites autour d'eux.</p>
+    </div>
+
+    <h4>II. Préambule</h4>
+    <div class="text">
+        <p>La Société invite ses Utilisateurs à lire attentivement les présentes Conditions Générales d’Utilisation (ci-après les " CGU "). Les CGU s'appliquent à tous les Services fournis par la Société à ses Utilisateurs d'une même catégorie, quelles que soient les clauses pouvant être inscrites sur les documents de l'Utilisateur.</p>
+
+        <p>Les CGU sont systématiquement communiquées à l'Utilisateur qui en fait la demande.</p>
+    </div>
+
+    <h4>III. Définitions</h4>
+    <div class="text">
+        <p>" Conditions Générales d'Utilisation " ou " CGU " désigne les présentes conditions générales d'utilisation en ligne ;</p>
+
+        <p>" Services " désigne toutes les prestations de service proposées aux Utilisateurs par la Société à travers le présent Site ;</p>
+
+        <p>" Site " désigne le présent Site, c’est-à-dire massage-moricet.fr;</p>
+
+        <p>" Société " désigne la Société 5C, plus amplement désignée à l'article I des présentes ; et
+
+        <p>" Utilisateur " désigne toute personne qui fait utilisation du Site.</p>
+    </div>
+
+    <h4>IV. Prestation de service</h4>
+    <div class="text">
+        <p>Les Services faisant l’objet des CGU sont ceux qui figurent sur le Site et qui sont proposés directement par la Société ou ses prestataires partenaires.</p>
+
+        <p>Les services sont décrits sur la page correspondante au sein du Site et mention est faite de toutes leurs caractéristiques essentielles. La Société ne peut être tenue responsable de l’impossibilité de réaliser ladite prestation lorsque l’Utilisateur n’est pas éligible.</p>
+    </div>
+
+    <h4>V. Cookies</h4>
+    <div class="text">
+        <p>Pour permettre à ses Utilisateurs de bénéficier d’une navigation optimale sur le Site et d’un meilleur fonctionnement des différentes interfaces et applications, la Société est susceptible d’implanter un cookie sur l’ordinateur de l’Utilisateur. Ce cookie permet de stocker des informations relatives à la navigation sur le Site.</p>
+        <p>L’Utilisateur autorise expressément la Société à déposer sur le disque dur de l’utilisateur un fichier dit« cookie ».</p>
+        <p>L’Utilisateur dispose de la possibilité de bloquer, modifier la durée de conservation, ou supprimer ce cookie via l’interface de son navigateur. Si la désactivation systématique des cookies sur le navigateur de l’Utilisateur l’empêche d’utiliser certains services ou fonctionnalités du Site, ce dysfonctionnement ne saurait en aucun cas constituer un dommage pour le membre qui ne pourra prétendre à aucune indemnité de ce fait.</p>
+    </div>
+
+    <h4>VI. Modifications </h4>
+    <div class="text">
+        <p>La Société se réserve le droit de modifier le Site, les services qui y sont proposés, les CGU ainsi que toute procédure de livraison ou autre élément constitutif des prestations réalisées par la Société par le biais du Site.</p>
+    </div>
+
+    <h4>VII. Responsabilité</h4>
+    <div class="text">
+        <p>La Société ne peut en aucun cas être tenue responsable pour l’indisponibilité, qu’elle soit temporaire ou permanente du Site Internet et, bien qu’elle mette en œuvre tous les moyens afin d’assurer en permanence le service, il se peut que celui-ci soit interrompu à tout moment. En outre, la Société se réserve le droit, par un acte volontaire, de rendre le Site indisponible afin de réaliser toute opération de mise à jour, d’amélioration ou de maintenance.</p>
+
+        <p>Tel que mentionné auparavant dans les présentes, la Société ne peut en aucun cas être tenue responsable des retards de réalisation d’une prestation de service pour des motifs qui échappent à son contrôle, indépendants de sa volonté, imprévisibles et irrésistibles ou dont la faute ne peut lui être imputable.</p>
+    </div>
+
+    <h4>VIII. Propriété intellectuelle</h4>
+    <div class="text">
+        <p>La marque, le logo, et la charte graphique du présent Site sont des marques déposées auprès de l'INPI et des oeuvres protégées au titre de la propriété intellectuelle, dont la propriété revient exclusivement à la Société. Toute diffusion, exploitation, représentation, reproduction, qu’elle soit partielle ou intégrale sans l’autorisation expresse de ladite société exposera le contrevenant à des poursuites civiles et pénales.</p>
+    </div>
+</div>
