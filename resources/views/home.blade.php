9057790c446c0405e5faacc2a956cb20fd254f33
camillenaulet03 <camille.naulet@hotmail.fr>
2020-02-11 15:31:08 +0100

    Description et video
    Intégration d'une video et ajouter une description du site sur la home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="home">
-    <p>home</p>
+    <iframe class='video' width="560" height="315" src="https://www.youtube.com/embed/CEHqMeCVhIs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <p class="description">Moment de détente ultime pour lâcher-prise et faire le plein d'énergie, le massage bénéficie de techniques ancestrales tirées de la médecine chinoise traditionnelle, de l'ayurveda -médecineindienne- ou encore du Japon, de Bali ou encore de la Thaïlande. Véritable sas de décompression, le massage nous transporte dans un autre espace temps où l'art du geste est roi. Pour que le soin soit efficace, on choisira un soin en fonction de vos problématiques ou de vos envies. Venez découvrir nos différents produits dans notre institut.</p>
 </div>
 @endsection
