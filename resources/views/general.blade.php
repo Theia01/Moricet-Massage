640763080fee683848df683ce11d43de28afa6af
maeldebon <mael.dbn@gmail.com>
2020-02-11 16:35:23 +0100

    Create footer

--- resources/views/general.blade.php
+++ resources/views/general.blade.php
@@ -4,15 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
 
         <title>Moricet Massage</title>
-    </head>
     <body>
 
     @include('header')
 
     @section('content')
     @show
+
+    @include('footer')
 
     </body>
 </html>
