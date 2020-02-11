30840a7c507bda2024e3301882f8f79d6ca9d185
maeldebon <mael.dbn@gmail.com>
2020-02-11 16:35:23 +0100

    Create footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="footer">
+    <ul>
+        <li><a href="{{ route("home") }}">Plan du site</a></li>
+        <li><a href="{{ route("home") }}">CGU</a></li>
+        <li><a href="{{ route("contact") }}">Contact</a></li>
+        <li><a class="social first" href="http://twitter.com">Twitter</a></li>
+        <li><a class="social" href="http://linkedin.com">LinkedIn</a></li>
+        <li><a class="social" href="http://facebook.com">Facebook</a></li>
+        <li><a class="social" href="http://instagram.com">Instagram</a></li>
+    </ul>
+</div>
