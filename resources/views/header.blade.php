ea4e2c0bf78f494c95eb41502dfda7e884f241f7
maeldebon <mael.dbn@gmail.com>
2020-02-12 12:15:46 +0100

    Add logo

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,5 @@
 <div class="header">
-    <img class="logo" src="test.png" alt="logo">
+    <img class="logo" src="logo.png" alt="logo">
     <ul>
         <li><a href="{{ route("home") }}">Accueil</a></li>
         <li><a href="{{ route("product") }}">Produits</a></li>
