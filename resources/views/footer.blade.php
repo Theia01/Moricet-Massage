9b8791140ad26b140787f5d18bcaa7057776a99c
maeldebon <mael.dbn@gmail.com>
2020-02-12 10:57:47 +0100

    Add target blank, change color, cgu link

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,25 +1,25 @@
 <div class="footer">
     <ul>
         <li><a href="{{ route("home") }}">Plan du site</a></li>
-        <li><a href="{{ route("home") }}">CGU</a></li>
+        <li><a href="legal/cgu.php" target="_blank">CGU</a></li>
         <li class="last"><a href="{{ route("contact") }}">Contact</a></li>
         <li>
-            <a href="http://facebook.com" class="fa fa-facebook-official facebookli">
+            <a href="http://facebook.com" class="fa fa-facebook-official facebookli" target="_blank">
                 <span class="linktext">Facebook</span>
             </a>
         </li>
         <li>
-            <a href="http://instagram.com" class="fa fa-instagram instagramli">
+            <a href="http://instagram.com" class="fa fa-instagram instagramli" target="_blank">
                 <span class="linktext">Instagram</span>
             </a>
         </li>
         <li>
-            <a href="http://twitter.com" class="fa fa-twitter twitterli">
+            <a href="http://twitter.com" class="fa fa-twitter twitterli" target="_blank">
                 <span class="linktext">Twitter</span>
             </a>
         </li>
         <li>
-            <a href="http://linkedin.com" class="fa fa-linkedin linkedinli">
+            <a href="http://linkedin.com" class="fa fa-linkedin linkedinli" target="_blank">
                 <span class="linktext">LinkedIn</span>
             </a>
         </li>
