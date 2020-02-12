789a8ee857ad36adf0b3dc68bd54a0acd50d14f6
Axelle <guinaudeau.ion1516@gmail.com>
2020-02-12 13:53:19 +0100

    Séparer vues emails
    Après quelques rectification d’écriture, j'ai séparer les vues pour en faire une général qui sert de template pour les deux autres. Ce qui permet d'éviter la répétition et pour chaque nouveau mail, il seront quasi-identique

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,27 +4,27 @@
 <div class="contact">
     <div class="container">
 
-    <h2>Contactez nous</h2>
-    <p>Vous avez une questions, une suggestion ou une remarque. Prenez contact avec nous par ce formulaire</p>
+    <h2>Contactez-nous</h2>
+    <p>Vous avez une question, une suggestion ou une remarque ? Prenez contact avec nous via ce formulaire !</p>
 
     {!! Form::open(['route'=>'sendcontact']) !!}
         <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
             {!! Form::label('Prénom:') !!}
-            {!! Form::text('name', old('name'), ['class'=>'form-control', 'placeholder'=>'Entrer un nom']) !!}
+            {!! Form::text('name', old('name'), ['class'=>'form-control', 'placeholder'=>'Entrez votre prénom']) !!}
             <span class="text-danger">{{ $errors->first('name') }}</span>
         </div>
         <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
             {!! Form::label('Email:') !!}
-            {!! Form::text('email', old('email'), ['class'=>'form-control', 'placeholder'=>'Entrer un email']) !!}
+            {!! Form::text('email', old('email'), ['class'=>'form-control', 'placeholder'=>'Entrez votre adresse mail']) !!}
             <span class="text-danger">{{ $errors->first('email') }}</span>
         </div>
         <div class="form-group {{ $errors->has('message') ? 'has-error' : '' }}">
             {!! Form::label('Message:') !!}
-            {!! Form::textarea('message', old('message'), ['class'=>'form-control', 'placeholder'=>'Entrer un message']) !!}
+            {!! Form::textarea('message', old('message'), ['class'=>'form-control', 'placeholder'=>'Entrez votre message']) !!}
             <span class="text-danger">{{ $errors->first('message') }}</span>
         </div>
         <div class="form-group">
-            <button class="btn btn-success">Contact US!</button>
+            <button class="btn btn-success">Envoyer ce message</button>
         </div>
     {!! Form::close() !!}
 
--- resources/views/emails/email.blade.php
+++ resources/views/emails/email.blade.php
@@ -1,33 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <div class="cadre">
-        <p>Une nouvelle demande de contact a été enregistrée ! <br><br>
-            {{ $contact['name'] }} ( mailto:{{ $contact['email'] }}) vous a envoyé le message suivant :
-            <br><br>
-            {{ $contact['message'] }}
-        </p>
-    </div>
+@extends('emails.templateEmail')
 
-    <style>
-      .cadre{
-        margin: 15vh 15% 0px 15%;
-        padding: 50px;
-        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.25);
-        border-radius: 10px 10px 0px 0px;
-        background-color: #FFF;
-        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.25);
-        height: 85vh;
-      }
-
-      html{
-        background-color: #92DB6C;
-        font-family: 'Poppins', sans-serif;
-      }
-    </style>
-  </body>
-
+@section('content')
+    <p>Une nouvelle demande de contact a été enregistrée ! <br><br>
+        {{ $contact['name'] }} ( mailto:{{ $contact['email'] }}) vous a envoyé le message suivant :
+        <br><br>
+        {{ $contact['message'] }}
+    </p>
+@endsection
