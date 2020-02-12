497873c8f33885d8571d0b3fc589edc26bcbc1ce
camillenaulet03 <camille.naulet@hotmail.fr>
2020-02-12 14:15:13 +0100

    Emplacement et mise en page
    Ajout du responsive et modification de la mise en page en ajoutant une image et du texte

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,17 @@
 
 @section('content')
 <div class="home">
-    <iframe class='video' width="560" height="315" src="https://www.youtube.com/embed/CEHqMeCVhIs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <p class="description">Moment de détente ultime pour lâcher-prise et faire le plein d'énergie, le massage bénéficie de techniques ancestrales tirées de la médecine chinoise traditionnelle, de l'ayurveda -médecineindienne- ou encore du Japon, de Bali ou encore de la Thaïlande. Véritable sas de décompression, le massage nous transporte dans un autre espace temps où l'art du geste est roi. Pour que le soin soit efficace, on choisira un soin en fonction de vos problématiques ou de vos envies. Venez découvrir nos différents produits dans notre institut.</p>
-    <section id="slideshow">
+    <div class="description">
+        <iframe class='video' width="560" height="315" src="https://www.youtube.com/embed/CEHqMeCVhIs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <p class="presentation">Moment de détente ultime pour lâcher-prise et faire le plein d'énergie, le massage bénéficie de techniques ancestrales tirées de la médecine chinoise traditionnelle, de l'ayurveda -médecineindienne- ou encore du Japon, de Bali ou encore de la Thaïlande. Véritable sas de décompression, le massage nous transporte dans un autre espace temps où l'art du geste est roi. Pour que le soin soit efficace, on choisira un soin en fonction de vos problématiques ou de vos envies. Venez découvrir nos différents produits dans notre institut.</p>
+    </div>
+    <div class="localisation">
+        <p class="presentation">Vous pouvez actuellement nous trouver à l’adresse suivante:
+            <br>4 rue Emile Masson 44000, Nantes</p>
+        <img class="adresse" src="{{ asset('images/adresse.png') }}"  alt="adresse" />
+    </div>
+    <div class="carousel">
+        <section class="slideshow">
         <div class="container">
             <div class="c_slider"></div>
             <div class="slider">
@@ -28,8 +36,8 @@
                 </figure>
             </div>
         </div>
-
-        <span id="timeline"></span>
-    </section>
+        </section>
+        <p class="message">Notre institut vous accueil pour un moment de bien-être et de détente disposant de différentes salles de massage en fonction de vos envies.</p>
+    </div>
 </div>
 @endsection
