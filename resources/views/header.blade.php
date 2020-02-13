01788412f1fa40532bac08dc1e095cad26368be0
maeldebon <mael.dbn@gmail.com>
2020-02-13 15:28:00 +0100

    Change products name in header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,7 +2,7 @@
 <img class="logo" src="{{ asset("images/logo.png") }}" alt="logo">
     <ul>
         <li><a href="{{ route("home") }}">Accueil</a></li>
-        <li><a href="{{ route("product") }}">Produits</a></li>
+        <li><a href="{{ route("product") }}">Massages</a></li>
         <li><a href="{{ route("contact") }}">Contact</a></li>
     </ul>
 </div>
