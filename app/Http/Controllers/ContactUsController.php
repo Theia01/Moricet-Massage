b08b196f044b84b4d15fdd3ffebbe82629df1deb
Axelle <guinaudeau.ion1516@gmail.com>
2020-02-14 11:08:23 +0100

    Mail filter
    On pouvais ne pas mettre de ".com" à notre adresse email dans le formulaire, j'ai ajouter un autre "style" (il nomme de cette manière dans la documentation) donc "filter".
    
    Et au lieu d'enlever le token et se retrouver avec des variable "hack" ou inconnu, j'ai mis un only qui autorise seulement certaine variable.

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -23,20 +23,20 @@ class ContactUsController extends Controller
    {
         $this->validate($request, [
          'name' => 'required|min:2|max:30',
-         'email' => 'required|email',
+         'email' => 'required|email:filter',
          'message' => 'required|max:1000'
          ]);
 
         try {
             Mail::to('moricet-massage@gmail.com')
-            ->send(new Contact($request->except('_token')));
+            ->send(new Contact($request->only(['name', 'email', 'message'])));
         } catch (Exception $e) {
             //report($e);
             return redirect('contact')->with('error', 'Petit problème technique, veuillez réessayer plus tard.');
         }  
 
         Mail::to($request->email)
-            ->send(new ConfirmationContact($request->except('_token')));
+            ->send(new ConfirmationContact($request->only(['name', 'email', 'message'])));
 
         return redirect('contact')->with('success', 'Mail envoyé !');
    }
