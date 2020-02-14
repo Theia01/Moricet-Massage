fa2e8e4bdcfb6a3ed77723268dac5bd5a63323b6
camillenaulet03 <camille.naulet@hotmail.fr>
2020-02-14 16:01:53 +0100

    Correction seeder
    Ajout d'un id

--- database/seeds/TarifTableSeeder.php
+++ database/seeds/TarifTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TarifTableSeeder
 {
@@ -11,46 +13,55 @@ class TarifTableSeeder
     public function run()
     {
         DB::table('tarif')->insert([
+            'id' => 1,
             'prix' => 50,
             'duree' => 30,
         ]);
 
         DB::table('massage')->insert([
+            'id' => 2,
             'prix' => 90,
             'duree' => 60,
         ]);
 
         DB::table('massage')->insert([
+            'id' => 3,
             'prix' => 100,
             'duree' => 65,
         ]);
 
         DB::table('massage')->insert([
+            'id' => 4,
             'prix' => 120,
             'duree' => 70,
         ]);
 
         DB::table('massage')->insert([
+            'id' => 5,
             'prix' => 70,
             'duree' => 50,
         ]);
 
         DB::table('massage')->insert([
+            'id' => 6,
             'prix' => 90,
             'duree' => 60,
         ]);
 
         DB::table('massage')->insert([
+            'id' => 7,
             'prix' => 40,
             'duree' => 20,
         ]);
 
         DB::table('massage')->insert([
+            'id' => 8,
             'prix' => 70,
             'duree' => 40,
         ]);
 
         DB::table('massage')->insert([
+            'id' => 9,
             'prix' => 50,
             'duree' => 30,
         ]);
