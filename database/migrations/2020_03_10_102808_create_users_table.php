dcc259a7e62c963057618defabf4ed616ee0516b
Axelle <guinaudeau.ion1516@gmail.com>
2020-03-10 16:14:26 +0100

    remet en place les ancienne migrations
    Laravel avait déjà des migrations par défaut que nous avons malencontreusement supprimé. Je les ai remises car pour l'authentification j'en ai besoin

--- database/migrations/2020_03_10_102808_create_users_table.php
+++ database/migrations/2020_03_10_102808_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id')->autoIncrement();
+            $table->string("name");
+            $table->string("email")->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->timestamps();
-            $table->string("pseudo");
-            $table->string("email");
-            $table->string("password");
+            $table->rememberToken();
             $table->integer("role")->nullable();
             $table->string("avatar")->nullable();
             $table->boolean("active")->default(1);
