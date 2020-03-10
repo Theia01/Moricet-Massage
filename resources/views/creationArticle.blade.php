1598c9ea1072297f62b98f9fbe4a1b38fcd4b999
Dreeion <a.garault44@gmail.com>
2020-03-10 15:33:06 +0100

    EVOL#Ajout de la page de création d'article.

---
+++ resources/views/creationArticle.blade.php
@@ -0,0 +1,48 @@
+@extends('general')
+
+@section('content')
+<div class="contact">
+    <div class="container">
+
+    <h2>Contactez-nous</h2>
+    <p>Vous avez une question, une suggestion ou une remarque ? Prenez contact avec nous via ce formulaire !</p>
+
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
+
+    {!! Form::open(['route'=>'sendcontact']) !!}
+        <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
+            {!! Form::label('Prénom:') !!}
+            {!! Form::text('name', old('name'), ['class'=>'form-control', 'placeholder'=>'Entrez votre prénom']) !!}
+            <span class="text-danger">{{ $errors->first('name') }}</span>
+        </div>
+        <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+            {!! Form::label('Email:') !!}
+            {!! Form::text('email', old('email'), ['class'=>'form-control', 'placeholder'=>'Entrez votre adresse mail']) !!}
+            <span class="text-danger">{{ $errors->first('email') }}</span>
+        </div>
+        <div class="form-group {{ $errors->has('message') ? 'has-error' : '' }}">
+            {!! Form::label('Message:') !!}
+            {!! Form::textarea('message', old('message'), ['class'=>'form-control', 'placeholder'=>'Entrez votre message']) !!}
+            <span class="text-danger">{{ $errors->first('message') }}</span>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-success">Envoyer ce message</button>
+        </div>
+    {!! Form::close() !!}
+
+    </div>
+
+
+
+</div>
+@endsection
