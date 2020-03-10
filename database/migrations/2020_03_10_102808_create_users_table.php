33b496328198a38b063a6d4aa2361dda2ed6bad9
Axelle <guinaudeau.ion1516@gmail.com>
2020-03-10 11:46:46 +0100

    add table users
    On ajoute une table user pour nos futur utilisateur

---
+++ database/migrations/2020_03_10_102808_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id')->autoIncrement();
+            $table->timestamps();
+            $table->string("pseudo");
+            $table->string("email");
+            $table->string("password");
+            $table->integer("role")->nullable();
+            $table->string("avatar");
+            $table->boolean("active")->default(1);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
