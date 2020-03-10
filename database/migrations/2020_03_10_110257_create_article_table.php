3df5672529bfa4dc787c4b13f4fa2b8eb5199c9d
Johan DELEON <johan@doowup.fr>
2020-03-10 12:10:08 +0100

    Création de fichier migration
    Fichier migration générant la table "article"

---
+++ database/migrations/2020_03_10_110257_create_article_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("nom");
+            $table->string("image");
+            $table->text("corps");
+            $table->unsignedBigInteger('user');
+
+            $table->foreign('user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
