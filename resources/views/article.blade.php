5b8d114c949bdd7e4531fb9e643c80bc760a3571
maeldebon <mael.dbn@gmail.com>
2020-03-11 14:03:46 +0100

    Just created the style of the Blog page

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -2,14 +2,19 @@
 
 @section('content')
 
+<div class="spacer"></div>
 
 @foreach ($articles as $key => $article)
-    
-    <div>
-    <p> This is user {{ $article->pseudo }}</p>
-    <img src="{{$article->image}}"/>
-    <p> {{ truncate($article->corps,50) }} </p>
-    </div>
+    <a class="link" href="/articles/{{$article->id}}">
+        <div class="box">
+            <p class="title"> {{ $article->nom }}</p>
+            <div class="image-container">
+                <img class="image" alt="article image" src="{{$article->image}}"/>
+            </div>
+            <p class="content"> {{ truncate($article->corps,50) }} </p>
+            <p class="info"> Created the {{ date('d/m/Y', strtotime($article->created_at))}} by <span class="author">{{ $article->pseudo }}</span>.</p>
+        </div>
+    </a>
 @endforeach
 
 {{ $articles->links() }}
