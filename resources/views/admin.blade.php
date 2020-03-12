34ad14fb31b6ef0da180d22cc6842ef055ad7a9f
maeldebon <mael.dbn@gmail.com>
2020-03-12 12:59:32 +0100

    Oops...

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,9 +7,9 @@
 
     @foreach($article as $key =>$articles)
         <div class="article">
-            <p class="banane"><span class="article-i">Titre:</span> {{ $articles->nom }}</p>
-            <p class="banane"><span class="article-i">Auteur:</span> {{ $articles->name }}</p>
-            <p class="banane"><span class="article-i">Date:</span> {{ $articles->created_at }}</p>
+            <p><span class="article-i">Titre:</span> {{ $articles->nom }}</p>
+            <p><span class="article-i">Auteur:</span> {{ $articles->name }}</p>
+            <p><span class="article-i">Date:</span> {{ $articles->created_at }}</p>
             <div class="article-button">
                 <a href="articles/{{ $articles->id }}">
                     <button class="view">Voir</button>
