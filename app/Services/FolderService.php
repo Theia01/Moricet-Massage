7326da37b3b5eae2f514972a82d6b11438839394
Johan DELEON <johan@doowup.fr>
2020-03-12 11:25:08 +0100

    Update du service de Scan
    Fonctionne pour les images du dossier "articles"

--- app/Services/FolderService.php
+++ app/Services/FolderService.php
@@ -14,20 +14,19 @@ use File;
 class FolderService {
 
     static public function getAllMassageImage(){
-
+        return FolderService::findInFolder("/"."images/"."article");
     }
 
     static public function findInFolder($path){
         $all_files = [];
-        //Get a list of file paths using the glob function.
-        $path =  public_path().$path."\\"."images\\"."article";
-        $fileList = array_slice(scandir($path),2);
-        echo($path);
-        //Loop through the array that glob returned.
+        $scan_path =  public_path().$path;
+        $fileList = array_slice(scandir($scan_path),2);
         foreach($fileList as $filename){
-            array_push();
-            echo("<br><br>".$filename);
+            if ( exif_imagetype($scan_path.'/'.$filename) ){
+            array_push($all_files,$path."/".$filename);
+            }
         }
+        return $all_files;
     
     }
 
