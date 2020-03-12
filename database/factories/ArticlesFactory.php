7823866cb646610021a4617fd140fe97cb06b0b3
camillenaulet03 <camille.naulet@hotmail.fr>
2020-03-12 16:49:34 +0100

    Ajouter images dans la base de données
    Mettre dans la base de données des images aléatoires du dossier image

--- database/factories/ArticlesFactory.php
+++ database/factories/ArticlesFactory.php
@@ -3,13 +3,16 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Articles;
+use App\Services\FolderService;
 use App\Users;
 use Faker\Generator as Faker;
 
 $factory->define(Articles::class, function (Faker $faker) {
     return [
         'nom' => $faker->sentence(1),
-        'image' => $faker->sentence(1),
+        'image' => $faker->randomElement(
+            FolderService::getAllBlogImage()
+        ),
         'corps' => $faker->text,
         'user' => Users::all()->random()->id,
         'created_at' => $faker->dateTime,
--- database/factories/MassagesFactory.php
+++ database/factories/MassagesFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Massages;
+use App\Services\FolderService;
 use Faker\Generator as Faker;
 
 $factory->define(Massages::class, function (Faker $faker) {
@@ -10,7 +11,9 @@ $factory->define(Massages::class, function (Faker $faker) {
         'nom' => $faker->sentence(1),
         'prix' => $faker->numberBetween(10, 30),
         'resume' => $faker->text,
-        'image' => $faker->sentence(1),
+        'image' => $faker->randomElement(
+            FolderService::getAllMassageImage()
+        ),
         'produits' => $faker->sentence(2),
         'description' => $faker->text,
         'bienfaits' => $faker->text,
