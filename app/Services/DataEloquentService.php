53b3995e6f76812516a8fb88341d079f6cd53f65
axelle <guinaudeau.ion1516@gmail.com>
2020-03-12 15:57:10 +0100

    ajout d'une méthode dans le service
    Ajout d'une méthode pour modifier l'utilisateur.

--- app/Services/DataEloquentService.php
+++ app/Services/DataEloquentService.php
@@ -150,7 +150,7 @@ static public function sendArticle(Request $request){
             'commentaires.'.Commentaires::CREATED_AT,
             "users.".Users::NAME,
             "users.".Users::AVATAR
-        )->leftJoin('users', 'users.id', '=', 'commentaires.'.Commentaires::USER)->where("commentaires.".Commentaires::ARTICLE,$id)->get();
+        )->leftJoin('users', 'users.id', '=', 'commentaires.'.Commentaires::USERS)->where("commentaires.".Commentaires::ARTICLE,$id)->get();
         ;
         return $com;
     }
@@ -172,4 +172,17 @@ static public function sendArticle(Request $request){
         return $art;
     }
 
+    static public function updateUser($id, $name, $email){
+        try {
+            
+            DB::table('users')->update(['name' => $name]);;
+
+        } catch (Exception $e) {
+
+            //report($e);
+            return false;
+
+        }
+        return "lol";
+    }
 }
