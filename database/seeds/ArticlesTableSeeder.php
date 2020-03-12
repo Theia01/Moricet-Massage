cc5c36a8e1db4b5a091468ecf737a8edc980a575
Johan DELEON <johan@doowup.fr>
2020-03-12 15:48:19 +0100

    Update Seeders

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -11,6 +11,6 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Articles::class, 20)->create();
+        //factory(App\Articles::class, 20)->create();
     }
 }
