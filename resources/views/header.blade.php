8550ed5e2c38b24d13223a1302ae06a3e653391b
maeldebon <mael.dbn@gmail.com>
2020-03-13 10:11:38 +0100

    Fix header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -8,28 +8,28 @@
 
         @guest
             <li>
-                <a class="hidden hide-login" href="{{ route('login') }}"><span>{{ __('Connexion') }}</span></a>
+                <a title="Se connecter" class="hidden hide-login" href="{{ route('login') }}"><span>{{ __('Connexion') }}</span></a>
             </li>
             @if (Route::has('register'))
                 <li>
-                    <a class="hidden hide-register" href="{{ route('register') }}"><span>{{ __('Inscription') }}</span></a>
+                    <a title="S'enregistrer" class="hidden hide-register" href="{{ route('register') }}"><span>{{ __('Inscription') }}</span></a>
                 </li>
             @endif
         @else
             {{-- Admin --}}
             @if(Auth::user()->role == 1)
                 <li>
-                    <a class="hidden hide-admin" href="{{ route('admin') }}"><span>Admin</span></a>
+                    <a title="Accéder au panel admin" class="hidden hide-admin" href="{{ route('admin') }}"><span>Admin</span></a>
                 </li>
             @endif
             <li>
-                <a href="{{ route('profil') }}"><span>Profil</span></a>
+                <a title="Accéder à votre profil" class="hidden hide-profile" href="{{ route('profil') }}"><span>Profil</span></a>
             </li>
             <li>
-                <a href="{{ route('logout') }}"
+                <a title="Se déconnecter" class="hidden hide-logout" href="{{ route('logout') }}"
                     onclick="event.preventDefault();
                                     document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
+                    <span>{{ __('Logout') }}</span>
                 </a>
 
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
