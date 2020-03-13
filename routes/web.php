f4a4c06787a80018569f1163b2c0396d6dd66c73
axelle <guinaudeau.ion1516@gmail.com>
2020-03-13 14:41:37 +0100

    Ajoute un groupe de route

--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,41 @@
 |
 */
 
+//HOME
 Route::get('/home', function () {return view('home');})->name('home');
 Route::redirect('/', '/home');
 
+//CONTACT
 Route::get('/contact', "ContactUsController@show")->name('contact');
 Route::post('/contact', "ContactUsController@send")->name('sendcontact');
 
-Route::get('/creationArticle', "CreationArticleController@show")->name('getAddArticle');
-Route::post('/creationArticle', "CreationArticleController@send")->name('postAddArticle');
 
+//PRODUIT
 Route::get('/produits', 'ArticleController@getProducts' )->name('product');
-
 Route::get('/produit/{id}', 'ArticleController@showProduct');
 
+//ARTICLE
 Route::get('/articles', 'BlogController@getArticles')->name('articles');
 Route::get('/articles/{id}', 'BlogController@showArticle');
 Route::post('/addcomment', 'BlogController@addComment')->middleware('auth')->name("addcomment");
 
-Route::get('/profil', 'ProfilController@show')->middleware('auth')->name('profil');
-Route::post('/profil', 'ProfilController@update')->middleware('auth')->name('updateprofil');
-
 Auth::routes();
-Route::get('/admin', 'AdminController@getArticle')->name('admin');
-Route::get('delete/{id}', 'AdminController@deleteArticle');
+
+//SI ON EST CONNECTER
+Route::group(['middleware' => ['auth']], function () {
+
+    //PROFIL
+    Route::get('/profil', 'ProfilController@show')->name('profil');
+    Route::post('/profil', 'ProfilController@update')->name('updateprofil');
+
+    //ADMIN
+    Route::get('/admin', 'AdminController@getArticle')->name('admin');
+    //supprime article
+    Route::get('delete/{id}', 'AdminController@deleteArticle');
+    //création d'article
+    Route::get('/creationArticle', "CreationArticleController@show")->name('getAddArticle');
+    Route::post('/creationArticle', "CreationArticleController@send")->name('postAddArticle');
+    
+});
+
+
