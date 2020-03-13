3c521d4546747585437242e1e263650a0f18499f
axelle <guinaudeau.ion1516@gmail.com>
2020-03-13 11:39:29 +0100

    Ajout de la méthode addComment dans le controller BlogController
    J'ai vérifier le si l'id était bon pour insérer dans la base de donnée et également le message
    
    Si il y a un problème on nous renvois un message d'erreur
    
    J'ai pensé également a ajouter la page 404 si les utilisateur demandait un id d'article qui n'existe pas

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -5,6 +5,8 @@ use App\Services\DataEloquentService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Route;
+use Auth;
+
 class BlogController extends Controller
 {
     public function getArticles(){
@@ -16,7 +18,32 @@ class BlogController extends Controller
         $id = Route::current()->parameter('id');
         $art = DataEloquentService::getOneArticle($id);
         $comments = DataEloquentService::getCommentsFromArticle($id);
-        return view('one_article', ['article'=>$art,'commentaires'=>$comments ] );
+        if($art == NULL){ //si l'utilisateur demande un article qui n'existe pas
+            return view('errors.404');
+        }else{
+            return view('one_article', ['article'=>$art,'commentaires'=>$comments ] );
+        }
     }
 
+    public function addComment(Request $request){
+        $id_article = $request->id_article;
+
+        //vérification de l'existence de l'article
+        $art = DataEloquentService::getOneArticle($id_article);
+        if($art == NULL){
+            return view('errors.404');
+        }
+
+        $this->validate($request, [
+                'message' => 'required|max:1000'
+            ]);
+
+        $addcomment = DataEloquentService::addcomment($id_article, Auth::user()->id, $request->message);
+
+        if($addcomment){
+            return redirect('/articles/'.$id_article);
+        }else{
+            return redirect('/articles/'.$id_article)->with('error', 'Petit problème technique, veuillez réessayer plus tard.');
+        } 
+    }
 }
